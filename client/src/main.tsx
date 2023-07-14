81abf2114ddffb0036bdbd79a7134a421ad9a5ba
mwongess <amosmwongelah@gmail.com>
2023-07-14 13:06:51 +0300

    def types

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { persistor, store } from "./redux/store/store.ts";
+import { persistor, store } from "./redux/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";
 import "./index.css";

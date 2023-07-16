774d2bedec4a46940706b2d3ab34e57021701133
mwongess <amosmwongelah@gmail.com>
2023-07-16 23:52:10 +0300

    feat: add db codes

--- server/src/controllers/auth.controllers.ts
+++ server/src/controllers/auth.controllers.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from "express";
+import { LoginSchema } from "../schemas/auth.schemas";
 
-export const login = async(req: Request, res:Response) => {
-    try {
-        const {userName, password} = req.body 
-        // Validate req body
-    } catch (error) {
-        res.json(error)
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { userName, password } = req.body;
+    // Validate req body
+    const { value, error } = LoginSchema.validate(req.body);
+    if (error) {
+      return res.json(error);
     }
-}
+    // const {recordset} = conne
+  } catch (error) {
+    res.json(error);
+  }
+};
 
-export const signup = async(req: Request, res:Response) => {
-    try {
-        const {userName,email, password} = req.body
-        // Validate req body
-    } catch (error) {
-        res.json(error)
-    }
-}
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { userName, email, password } = req.body;
+    // Validate req body
+  } catch (error) {
+    res.json(error);
+  }
+};

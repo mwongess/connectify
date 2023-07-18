792a593fa8d4192c9063e27afeda983f67ca046f
mwongess <amosmwongelah@gmail.com>
2023-07-18 17:06:28 +0300

    update mobile responsiveness

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -6,12 +6,12 @@ import CreatePost from "./post/CreatePost";
 const Home = () => {
   return (
     <>
-      <div className="flex justify-between gap-2">
-        <div className="w-[70%]">
+      <div className="sm:flex justify-between gap-2">
+        <div className="p-3 sm:0 w-full sm:w-[70%]">
           <CreatePost />
           <NewsFeed />
         </div>
-        <div className="w-[30%] rounded-[10px] h-fit bg-white p-3" >
+        <div className="hidden sm:block w-[30%] rounded-[10px] h-fit bg-white p-3" >
           <input
             className="border border-blue-500 w-full rounded p-2 bg-transparent"
             type="search"
--- client/src/layouts/HomeLayout.tsx
+++ client/src/layouts/HomeLayout.tsx
@@ -5,10 +5,10 @@ const HomeLayout = () => {
   return (
     <div className="bg-[#e5e7eb]">
       <div className="flex border">
-        <div className="w-[20%] p-3 ">
+        <div className="hidden sm:block w-[20%] p-3 ">
           <SideNav />
         </div>
-        <div className="w-[80%] p-3">
+        <div className="w-full sm:w-[80%] sm:pr-3 sm:pt-3">
           <Outlet />
         </div>
       </div>
